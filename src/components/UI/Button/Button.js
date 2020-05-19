5c8ed4d07967f27fe7600ed7017e7c8310a68098
liliuhua <liliuhua0306@gmail.com>
2020-05-18 21:36:44 -0400

    Basic1.0

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './Button.module.css';
+
+const button = (props) => (
+    <button
+        className={[classes.Button, classes[props.btnType]].join(' ')} //join this with a whitespace to have a list of classes
+        onClick={props.clicked}>{props.children}</button>
+);
+
+export default button
