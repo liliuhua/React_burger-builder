2308eb82256154a935e180b2148f3a3309e6d680
liliuhua <liliuhua0306@gmail.com>
2020-05-19 23:08:17 -0400

    Basic2.0

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,7 +7,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
-    salad: 0.5,
+    salad: 0.6,
     cheese: 0.4,
     meat: 1.3,
     bacon: 0.7
