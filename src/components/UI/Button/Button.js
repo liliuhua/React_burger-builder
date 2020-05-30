98075304fc9095e5bbc1b58f56f6a9556609e106
liliuhua <liliuhua0306@gmail.com>
2020-05-30 12:46:46 -0400

    Basic2.0

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -4,6 +4,7 @@ import classes from './Button.module.css';
 
 const button = (props) => (
     <button
+        disabled={props.disabled}
         className={[classes.Button, classes[props.btnType]].join(' ')} //join this with a whitespace to have a list of classes
         onClick={props.clicked}>{props.children}</button>
 );
